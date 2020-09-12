dac1deddec3084d96793ae3a5cbb05bec9eb86b9
Rafael Miziara <rafaelmiziara@hotmail.com>
2020-09-13 00:25:11 +0200

    Fixing express

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
 import passport from 'passport';
+import path from 'path';
 
 import './providers/passport';
 
@@ -21,5 +22,17 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use(router);
+
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assests
+  // like our main.js, or main.css file
+  app.use(express.static('client/build'));
+
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 export { app };
