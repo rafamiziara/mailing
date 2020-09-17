9623c1a97c0da5ce1cbc4c8e603a0bdc4d34d455
Rafael Miziara <rafaelmiziara@hotmail.com>
2020-09-18 00:29:03 +0200

    Changing app name

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -4,7 +4,7 @@ const Landing = () => {
   return (
     <div style={{ textAlign: 'center' }}>
       <h1>
-        Emaily!
+        mailing
       </h1>
       Collect feedback form your users
     </div>
