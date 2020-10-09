97b7f0ab5a664256dc59665a69d10bba49cc4346
Rafael Miziara <rafaelmiziara@hotmail.com>
2020-10-09 10:41:45 +0200

    Limiting recipients list

--- client/src/utils/validateEmails.js
+++ client/src/utils/validateEmails.js
@@ -2,10 +2,16 @@
 const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
 export default (emails) => {
-  const invalidEmails = emails
+  const emailsArray = emails
     .split(',')
-    .map(email => email.trim())
-    .filter(email => email && re.test(email) === false)
+    .map(email => email.trim());
+
+  if(emailsArray.length > 5) {
+    return 'As a test project, you cannot send more than 5 emails at a time.'
+  }
+  
+  const invalidEmails = emailsArray
+    .filter(email => email && re.test(email) === false);
 
   if(invalidEmails.length) {
     return `These emails are invalid: ${invalidEmails}`;
