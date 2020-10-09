ad7673b69caf1b019d9ecc477947845fda0f3479
Rafael Miziara <rafaelmiziara@hotmail.com>
2020-10-09 12:41:43 +0200

    Adding credit card alert

--- src/useCases/createCharge/CreateChargeUseCase.ts
+++ src/useCases/createCharge/CreateChargeUseCase.ts
@@ -9,10 +9,10 @@ export class CreateChargeUseCase {
     const sessionId = await this.paymentsProvider.createCheckout(
       [
         {
-          name: 'Add Credits',
+          name: 'Add 5 Credits',
           amount: 500,
           currency: 'usd',
-          description: '$5 for 5 credits',
+          description: 'Genuine card information cannot be used in test mode. Instead, use the test card number: 4242 4242 4242 4242, a valid expiration date in the future, and any random CVC number, to create a successful payment.',
           quantity: 1,
         },
       ],
